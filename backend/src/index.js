12189539d7c83decf5a5ddd3049a2b6bcce8b2c1
NwobiaDavid <dnwobia@gmail.com>
2024-06-13 23:08:02 +0100

    linked the backend with the frontend

---
+++ backend/src/index.js
@@ -0,0 +1,44 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import cors from 'cors';
+import express from 'express';
+import mongoose from 'mongoose';
+import path from 'path';
+
+const app = express();
+
+const MONGODB_URI = process.env.MONGO_URI || 'mongodb:/localhost/crushers'
+
+const __dirname = path.resolve();
+
+mongoose.set('strictQuery', true);
+
+mongoose.connect(MONGODB_URI)
+.then(()=>{
+    console.log('connected to mongoDB 💥💥💥');
+})
+.catch(()=>{
+    console.log('error occurred during connection :(');
+})
+
+app.use(
+    cors({
+        credentials: true,
+        origin: true
+    })
+)
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+const PORT = parseInt((process.env.PORT || '4000'), 10)
+
+//production path
+// app.use(express.static(path.join(__dirname, '../../frontend/dist')))
+// app.get('*', (req, res)=>
+//     res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'))
+// )
+
+app.listen(PORT, ()=>{
+    console.log(`listening on port ${PORT}...`)
+})
