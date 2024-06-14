b3aecb433b7176c7a4761c22e91ffc8566375eff
Skeby <103804946+Skeby@users.noreply.github.com>
2024-06-15 00:21:14 +0100

    deploy-fix: remove unused import

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,15 +1,15 @@
 import { Button } from "@/components/ui/button";
-import { useAppDispatch, useAppSelector } from "@/state/store";
-import { UserLoginFields, UserLoginSchema } from "@/static/schema";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+// import { useAppDispatch, useAppSelector } from "@/state/store";
+// import { UserLoginFields, UserLoginSchema } from "@/static/schema";
+// import { zodResolver } from "@hookform/resolvers/zod";
+// import { useForm } from "react-hook-form";
 
 const Login = () => {
-  const dispatch = useAppDispatch();
-  const { user } = useAppSelector((state) => state.auth);
-  const methods = useForm<UserLoginFields>({
-    resolver: zodResolver(UserLoginSchema),
-  });
+  // const dispatch = useAppDispatch();
+  // const { user } = useAppSelector((state) => state.auth);
+  // const methods = useForm<UserLoginFields>({
+  //   resolver: zodResolver(UserLoginSchema),
+  // });
   // const { handleSubmit, register } = methods;
   return (
     <div>
