ad78c8199bc412337ceced3b318581414ec496c0
Agbakwuru Oluchi <152165721+oluchicharity@users.noreply.github.com>
2024-06-16 23:05:14 +0100

    added functions to view one and all

--- backend/src/Routes/StudentRoute.js
+++ backend/src/Routes/StudentRoute.js
@@ -1,10 +1,12 @@
 import express from 'express'
 
-import { LoginStudent, RegisterStudent } from '../Controllers/StudentController.js'
+import { GetAllStudents, GetStudentById, LoginStudent, RegisterStudent } from '../Controllers/StudentController.js'
 
 const StudentRouter = express.Router()
 
 StudentRouter .post('/registerStudent', RegisterStudent)
 StudentRouter .post('/loginStudent', LoginStudent );
+StudentRouter.get('/getStudentById/:studentId', GetStudentById)
+StudentRouter.get('/getAllStudents', GetAllStudents)
 
 export default StudentRouter ;
