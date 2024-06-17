14aba598394a4aa2a05a8dd77c95f19debf60b8f
Agbakwuru Oluchi <152165721+oluchicharity@users.noreply.github.com>
2024-06-17 09:42:51 +0100

    added a view one and all to exams

--- backend/src/Controllers/ExamController.js
+++ backend/src/Controllers/ExamController.js
@@ -38,3 +38,31 @@ export const createExam = async (req, res) => {
   }
 };
 
+export const GetExamById = async (req, res) => {
+  try {
+      const examId = req.params.examId; 
+
+      const exam = await Exam.findById(examId);
+      if (!exam) {
+          return res.status(404).json({ message: 'Exam not found' });
+      }
+      res.status(200).json(exam);
+  } catch (error) {
+      console.error('Error fetching exam by ID:', error);
+      res.status(500).json({ message: 'Server error' });
+  }
+};
+
+export const GetAllExams = async (req, res) => {
+  try {
+      const exam = await Exam.find();
+
+      if (exam.length === 0) {
+          return res.status(404).json({ message: 'No exams found' });
+      }
+      res.status(200).json(exam);
+  } catch (error) {
+      console.error('Error fetching all exam:', error);
+      res.status(500).json({ message: 'Server error' });
+  }
+};
