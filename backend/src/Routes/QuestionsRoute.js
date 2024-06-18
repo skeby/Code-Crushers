ace85414b9c81e64bc78fad995d9dd02e0bc7651
NwobiaDavid <dnwobia@gmail.com>
2024-06-18 23:20:19 +0100

    feat: minor case change

--- backend/src/Routes/QuestionsRoute.js
+++ backend/src/Routes/QuestionsRoute.js
@@ -4,9 +4,9 @@ import { createTheoryQuestion } from '../Controllers/TheoryQuestionController.js
 import { createObjectiveQuestion } from '../Controllers/ObjectiveQuestionController.js'
 import authMiddleware, {} from '../Middlewares/Authorization.js'
 
-const Questionrouter = express.Router()
+const QuestionRouter = express.Router()
 
 Questionrouter.post('/createTheory',authMiddleware,createTheoryQuestion )
 Questionrouter.post('/createObjective',authMiddleware, createObjectiveQuestion );
 
-export default Questionrouter;
+export default QuestionRouter;
