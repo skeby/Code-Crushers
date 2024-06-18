0ec484745bfc32c50955e1f49c03a83f22ff533f
Agbakwuru Oluchi <152165721+oluchicharity@users.noreply.github.com>
2024-06-18 15:56:48 +0100

    Update TeacherController.js

--- backend/src/Controllers/TeacherController.js
+++ backend/src/Controllers/TeacherController.js
@@ -43,7 +43,7 @@ export const RegisterTeacher = async (req, res) => {
   res.status(201).json({ message: `${role} registered successfully!`, userWithoutPassword});
 } catch (error) {
   console.error('Error registering teacher:', error);
-  res.status(500).json({ message: 'Server error', error});
+  res.status(500).json(error.message);
   }};
   
 
